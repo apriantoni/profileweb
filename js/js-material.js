f26feb558a7d8e8be9979ef418cf307ed4047dcc
Apriantoni Nadiwaryo <apriantoninadiwaryo@gmail.com>
2020-05-09 10:33:30 +0700

    simpleweb
    -

---
+++ js/js-material.js
@@ -0,0 +1,34 @@
+function home_text(){
+	alert('Hallo! Ini adalah menu Beranda.');
+}
+
+function information_text(){
+	alert('Hallo! Ini adalah menu Informasi.');
+}
+
+function portofolio_text(){
+	alert('Hallo! Ini adalah menu Portofolio.');
+}
+
+function biodata_text() {
+	alert(	'Biodata : \n'+
+			'Nama : Apriantoni \n' +
+			'Alamat : Jl. Perintis Kemerdekaan no. 46, Meranto Dusun VIII, Kec. Meranti, Kab, Asahan, Sumater Utara \n' +
+			'Jenis Kelamin : Laki-laki \n'+
+			'Agama : Islam \n' +
+			'dll..');
+}
+
+function education_text() {
+	alert(	'Pendidikan : \n'+
+			'D-IV Sistem Informasi Politeknik Caltex Riau, 2014-2018 \n' +
+			'SMA Negeri 1 Meranti, 2011-2014 \n' +
+			'dll..');
+}
+
+function experience_text() {
+	alert(	'Pengalaman Kerja : \n'+
+			'- Freelance Software Engineer, 2016-2019 \n' +
+			'- Programmer in Pekanbaru Major Office, 2019 \n' +
+			'- Web Developer Internship in DISKOMINFOTIKSAN Pekanbaru, 2017');
+}
